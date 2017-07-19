7ad15f4069c46a2b07693c380118af323d2aa514
José Alvis <jalvis1491@gmail.com>
2017-07-19 12:13:01 -0400

    Bien
    Te  corregí los errores que pude encontrar.

---
+++ Tarea.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict'
+
+
+var Nombre = "Yanzi Cuevas"; 
+var Edad ="26"; 
+var Profesion = "Est. Informatica"; 
+var Nivelprog ="2"; 
+var ExpectativasAngularJS= "Desarrollar habilidades en las apps web";
+
+//Te falto imprimir las variables
+//Si escribes console.log("Esto es un Texto.") solo te va a impimir :  Esto es un Texto.
+//El mismo caso para  console.log("Nombre");  solo va a imprimir: Nombre 
+
+//correción
+console.log("Nombre:", Nombre); 
+console.log("Edad:",Edad);
+console.log("Profesion:", Profesion);
+console.log("Nivelprog:", Nivelprog);
+console.log("ExpectativasAngularJS:", ExpectativasAngularJS);
+
+
+
+})();
